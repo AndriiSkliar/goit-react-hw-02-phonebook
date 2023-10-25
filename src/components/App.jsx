5fd8b020a00d9f0e4436311221d1acaad03696e3
Андрей Скляр <andreas.skliar@gmail.com>
2023-10-25 13:20:20 +0300

    base commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react"
+export class App extends Component {
+  state = {
+    inputValue: '123',
+  }
+
+  render() {
+
+    return (
+      <div>
+        <input type="text" value={this.state.inputValue}/>
+      </div>
+    );
+  }
 };
